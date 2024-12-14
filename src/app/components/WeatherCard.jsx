e297917cf59d6acd420b9b0aae93909d6dbbf2cd
Gunjlkham08 <gunjegunje54@gmail.com>
2024-12-14 17:46:33 +0800

    added some changes

--- src/app/components/WeatherCard.jsx
+++ src/app/components/WeatherCard.jsx
@@ -1,10 +1,15 @@
 
-export default function WeatherCard({ bgcolor }) {
+export default function WeatherCard({ bgcolor, date, cityName, temperature, stat}) {
   return (
-    <div
-      className={`w-[345px] h-[750px] rounded-3xl absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${
-        bgcolor === "white" ? "bg-white" : "bg-black"
-      }`}
-    ></div>
+    <div className={`w-[345px] h-[750px] px-7 py-10 rounded-3xl absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${
+        bgcolor === "white" ? "bg-white" : "bg-black" }`} >
+       <div className='text-gray-500 text-xl'>{date}</div>
+       <div className='text-[48px] text-black'>{cityName}</div>
+       <div className='flex justify-center mt-10'>
+         <img src='/sun.png' width='262' height='262'/>
+       </div>
+       <div className='text-9xl mt-7'>{temperature}</div>
+       <div className='text-2xl'>{stat}</div>
+    </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,26 +2,26 @@
 import WeatherCard from './components/WeatherCard';
 import Circle from './components/circle';
 import Logo from './components/decorations';
-import {FilledCircle} from './components/decorations';
-// import {IoIosSearch} from "react-icons/io"
+import { AiOutlineSearch } from "react-icons/ai";
 
 export default function Home() {
   return (
     <div className="flex h-screen w-full bg-white justify-center relative">
       <div className="w-[1200px] h-full flex relative">
         <div className="w-1/2 h-full bg-[#F3F4F6] relative">
-         <div className="w-[400px] h-[55px] bg-white rounded-3xl flex items-center">
-           <input className="p-2" placeholder="Search" />
+         <div className="w-[400px] h-[55px] bg-white rounded-3xl flex items-center ml-10 mt-10">
+           <AiOutlineSearch className='text-2xl ml-5'/>
+           <input className="p-3 border-none text-xl" placeholder="Search"/>
          </div>
-         <WeatherCard bgcolor="white" />
+         <WeatherCard bgcolor="white" date='Sep 10, 2024' cityName='Ulaanbaatar' temperature='12°' stat='bright'/>
         </div>
         <div className="w-1/2 h-full bg-[#0F141E] relative">
-          <WeatherCard bgcolor="black" />
+          <WeatherCard bgcolor="black"  date='Sep 10, 2024' cityName='Krakow' stat='clear'/>
         </div>
         <Circle size={140} />
         <Circle size={340} />
         <Circle size={540} />
-        <FilledCircle/>
+        <Circle size={940} />
       </div>
       <Logo/>
     </div>
